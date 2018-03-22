ecb6900b0450942f7114f5acddd401abad194181
Poy Chang <poypost@gmail.com>
2018-03-22 15:11:11 +0800

    formate code layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,21 @@
 var dev = process.env.NODE_ENV != 'production';
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var extractCSS = new ExtractTextPlugin('app.css');
 
 module.exports = {
-  entry: __dirname + "/src/app.scss",
-  output: {
-    path: __dirname + "/dist/app",
-    filename: "app.js"
-  },
+  entry: __dirname + '/src/app.scss',
+  output: {path: __dirname + '/dist/app', filename: 'app.js'},
   module: {
     rules: [
+      {test: /\.css$/, use: extractCSS.extract(['css-loader'])},
       {
-        test: /\.css$/,
-        use: extractCSS.extract([ 'css-loader' ])
-      },
-      {
-       test: /\.scss$/,
-       use: extractCSS.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
+        test: /\.scss$/,
+        use: extractCSS.extract(
+            {fallback: 'style-loader', use: ['css-loader', 'sass-loader']})
       },
     ]
   },
-  resolve: {
-    modules: ["node_modules"]
-  },
-  plugins: [
-    extractCSS
-  ]
+  resolve: {modules: ['node_modules']},
+  plugins: [extractCSS]
 };
